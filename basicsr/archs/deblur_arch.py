d0de7a76799ddd1da2bf54710a4e4b999cf181f5
Drealizem <1014269733@qq.com>
2024-10-22 16:37:33 +0800

    Update deblur_arch.py

--- basicsr/archs/deblur_arch.py
+++ basicsr/archs/deblur_arch.py
@@ -13,23 +13,30 @@ from einops import rearrange
 
 @ARCH_REGISTRY.register()
 class Deblur(nn.Module):
+    """
+    视频去模糊模型，使用深度学习技术来恢复模糊视频中的清晰帧。
+
+    Args:
+        num_feat (int): 网络中特征图的通道数，默认为64。
+        num_block (int): 网络中使用的残差块的数量，默认为15。
+    """
     def __init__(self, num_feat=64, num_block=15):
         super().__init__()
         self.num_feat = num_feat
 
         # extractor & reconstruction
-        self.feat_extractor = nn.Conv3d(3, num_feat, (1, 3, 3), 1, (0, 1, 1), bias=True)
-        self.recons = nn.Conv3d(num_feat, 3, (1, 3, 3), 1, (0, 1, 1), bias=True)
+        self.feat_extractor = nn.Conv3d(3, num_feat, (1, 3, 3), 1, (0, 1, 1), bias=True)  # 特征提取器，用于从输入的模糊帧中提取特征
+        self.recons = nn.Conv3d(num_feat, 3, (1, 3, 3), 1, (0, 1, 1), bias=True)  # 重建模块，用于将提取的特征图重建为清晰的帧
 
         # wave tf
-        self.wave = HaarDownsampling(num_feat)
-        self.x_wave_1_conv1 = nn.Conv2d(num_feat * 3, num_feat * 3, 1, 1, 0, groups=3)
+        self.wave = HaarDownsampling(num_feat)  # 哈尔小波变换模块，用于在变换域中处理特征图
+        self.x_wave_1_conv1 = nn.Conv2d(num_feat * 3, num_feat * 3, 1, 1, 0, groups=3)  # 用于处理小波变换后的特征图的卷积层
         self.x_wave_1_conv2 = nn.Conv2d(num_feat * 3, num_feat * 3, 1, 1, 0, groups=3)
         # wave pro
-        self.x_wave_2_conv1 = nn.Conv2d(num_feat * 3, num_feat * 3, 1, 1, 0, groups=3)
+        self.x_wave_2_conv1 = nn.Conv2d(num_feat * 3, num_feat * 3, 1, 1, 0, groups=3)  # 另一组用于处理小波变换后的特征图的卷积层
         self.x_wave_2_conv2 = nn.Conv2d(num_feat * 3, num_feat * 3, 1, 1, 0, groups=3)
 
-        # transformer
+        # transformer # 变换域特征融合模块，使用 CWGDN 进行特征融合
         transformer_scale4 = []
         for _ in range(5):
             transformer_scale4.append(
@@ -37,39 +44,52 @@ class Deblur(nn.Module):
             )
         self.transformer_scale4 = nn.Sequential(*transformer_scale4)
 
-        # propogation branch
+        # propogation branch # 特征传播模块，用于在时间维度上传播特征
         self.forward_propagation = manual_conv3d_propagation_forward(num_feat, num_block)
         self.backward_propagation = manual_conv3d_propagation_backward(num_feat, num_block)
 
-        # activation functions
+        # activation functions # 激活函数
         self.lrelu = nn.LeakyReLU(negative_slope=0.1, inplace=True)
 
     def forward(self, lrs):
+        """
+        模型的前向传播函数，处理输入的模糊视频帧，并输出去模糊后的帧。
+        Args:
+            lrs (Tensor): 输入的模糊视频帧，形状为 (batch_size, time, channel, height, width)。
+        Returns:
+            Tensor: 去模糊后的视频帧，形状为 (batch_size, time, channel, height, width)。
+        """
         # time_start = time.time()
         # print(lrs.size())
-        b, t, c, h, w = lrs.size()
+        b, t, c, h, w = lrs.size()  # 时间维度上的特征提取
         lrs_feature = self.feat_extractor(rearrange(lrs, 'b t c h w -> b c t h w'))     # b c t h w
-        # scale1
+        # scale1 # 第一层小波变换
         tf_input_feature = rearrange(lrs_feature, 'b c t h w -> (b t) c h w')
         tf_wave1_l, tf_wave1_h = self.wave(tf_input_feature)
         tf_wave1_h = self.x_wave_1_conv2(self.lrelu(self.x_wave_1_conv1(tf_wave1_h)))
-        # scale2
+        # scale2 # 第二层小波变换
         tf_wave2_l, tf_wave2_h = self.wave(tf_wave1_l)
         tf_wave2_l = rearrange(self.transformer_scale4(rearrange(tf_wave2_l, '(b t) c h w -> b t c h w', b=b)), 'b t c h w -> (b t) c h w')
         tf_wave2_h = self.x_wave_2_conv2(self.lrelu(self.x_wave_2_conv1(tf_wave2_h)))
-        # scale1
+        # scale1 # 特征融合
         tf_wave1_l = self.wave(torch.cat([tf_wave2_l, tf_wave2_h], dim=1), rev=True)
         tf_wave1_l = rearrange(self.forward_propagation(self.backward_propagation(rearrange(tf_wave1_l, '(b t) c h w -> b t c h w', b=b))), 'b t c h w -> (b t) c h w')
-        pro_feat = rearrange(self.wave(torch.cat([tf_wave1_l, tf_wave1_h], dim=1), rev=True), '(b t) c h w -> b t c h w', b=b)
+        pro_feat = rearrange(self.wave(torch.cat([tf_wave1_l, tf_wave1_h], dim=1), rev=True), '(b t) c h w -> b t c h w', b=b)  # 重建去模糊帧
 
-        # reconstruction
+        # reconstruction # 输出去模糊后的帧
         out = rearrange(self.recons(rearrange(pro_feat, 'b t c h w -> b c t h w')), 'b c t h w -> b t c h w')
 
         # time_end = time.time()
         # print("inference time:", time_end - time_start)
         return out.contiguous() + lrs
 
-
+'''
+包含多个残差块的类，用于提取特征
+    num_feat: 特征图的通道数。
+    num_block: 残差块的数量。
+    make_layer: 创建指定数量的残差块。
+    forward: 将输入特征传递给残差块序列。
+'''
 class ResidualBlocks2D(nn.Module):
     def __init__(self, num_feat=64, num_block=30):
         super().__init__()
@@ -80,6 +100,16 @@ class ResidualBlocks2D(nn.Module):
         return self.main(fea)
 
 
+'''
+实现了一个3D卷积层，用于在时间维度上向后传播特征
+    num_feat: 特征图的通道数。
+    num_block: 残差块的数量。
+    conv1, conv2, conv3: 用于特征融合和处理的卷积层。
+    kernel_conv_pixel: 动态像素卷积层。
+    lrelu: 激活函数。
+    resblock_bcakward2d: 2D残差块。
+    forward: 实现特征的向后传播。
+'''
 class manual_conv3d_propagation_backward(nn.Module):
     def __init__(self, num_feat=64, num_block=15):
         super().__init__()
@@ -116,7 +146,16 @@ class manual_conv3d_propagation_backward(nn.Module):
         conv3d_feature = torch.stack(backward_list, dim=1)      # b 64 t 256 256
         return conv3d_feature
 
-
+'''
+实现了一个3D卷积层，用于在时间维度上向前传播特征。
+    num_feat: 特征图的通道数。
+    num_block: 残差块的数量。
+    conv1, conv2, conv3: 用于特征融合和处理的卷积层。
+    kernel_conv_pixel: 动态像素卷积层。
+    lrelu: 激活函数。
+    resblock_bcakward2d: 2D残差块。
+    forward: 实现特征的向前传播。
+'''
 class manual_conv3d_propagation_forward(nn.Module):
     def __init__(self, num_feat=64, num_block=15):
         super().__init__()
