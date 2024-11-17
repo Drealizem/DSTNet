c1728355b76978f4ff5a6987f94d95533c07f9c2
Drealizem <1014269733@qq.com>
2024-11-17 21:05:06 +0800

    Update deblur_arch.py

--- basicsr/archs/deblur_arch.py
+++ basicsr/archs/deblur_arch.py
@@ -55,7 +55,7 @@ class Deblur(nn.Module):    # 本文使用的就是这个模型
         """
         模型的前向传播函数，处理输入的模糊视频帧，并输出去模糊后的帧。
         Args:
-            lrs (Tensor): 输入的模糊视频帧，形状为 (batch_size, time, channel, height, width)。
+            lrs (Tensor): 输入的模糊视频帧（原始图像），形状为 (batch_size, time, channel, height, width)。
 
         Returns:
             Tensor: 去模糊后的视频帧，形状为 (batch_size, time, channel, height, width)。
@@ -66,16 +66,20 @@ class Deblur(nn.Module):    # 本文使用的就是这个模型
         lrs_feature = self.feat_extractor(rearrange(lrs, 'b t c h w -> b c t h w'))     # b c t h w
         # scale1 # 使用特征提取器从输入的低分辨率图像中提取特征
         tf_input_feature = rearrange(lrs_feature, 'b c t h w -> (b t) c h w')
-        tf_wave1_l, tf_wave1_h = self.wave(tf_input_feature)    # 第一次小波变换
+        tf_wave1_l, tf_wave1_h = self.wave(tf_input_feature)    # 第一次小波变换(分别输出低频和高频部分)
         tf_wave1_h = self.x_wave_1_conv2(self.lrelu(self.x_wave_1_conv1(tf_wave1_h)))   # 使用卷积层处理小波变换后的高频部分
         # scale2 # 对低频部分再次进行小波变换
         tf_wave2_l, tf_wave2_h = self.wave(tf_wave1_l)  # 第二次小波变换
-        tf_wave2_l = rearrange(self.transformer_scale4(rearrange(tf_wave2_l, '(b t) c h w -> b t c h w', b=b)), 'b t c h w -> (b t) c h w') # 使用变换域特征融合模块处理第二次小波变换后的低频部分
+        tf_wave2_l = rearrange(self.transformer_scale4(rearrange(tf_wave2_l, '(b t) c h w -> b t c h w', b=b)), 'b t c h w -> (b t) c h w') # 使用变换域特征融合模块(包含CWGDN)处理第二次小波变换后的低频部分
         tf_wave2_h = self.x_wave_2_conv2(self.lrelu(self.x_wave_2_conv1(tf_wave2_h)))   # 处理小波变换后的高频部分
         # scale1 # 将两次小波变换的结果进行融合
-        tf_wave1_l = self.wave(torch.cat([tf_wave2_l, tf_wave2_h], dim=1), rev=True)
+        tf_wave1_l = self.wave(torch.cat([tf_wave2_l, tf_wave2_h], dim=1), rev=True) # rev=True表示逆变换IWT；这里的tf_wave2_l已经完成了CWGDN；IWT的结果存入tf_wave1_l中
+        # ***重点*** 这里就是WaveletFP模块（这里时间上先向后再向前传播）
         tf_wave1_l = rearrange(self.forward_propagation(self.backward_propagation(rearrange(tf_wave1_l, '(b t) c h w -> b t c h w', b=b))), 'b t c h w -> (b t) c h w') # 使用特征传播模块在时间维度上传播特征
-        pro_feat = rearrange(self.wave(torch.cat([tf_wave1_l, tf_wave1_h], dim=1), rev=True), '(b t) c h w -> b t c h w', b=b)  # 将传播后的特征与高频部分结合，进行小波逆变换，重建去模糊帧
+        '''可将上一行替换成别的模块'''
+
+        # 将传播后的特征与高频部分结合，进行小波逆变换，重建去模糊帧
+        pro_feat = rearrange(self.wave(torch.cat([tf_wave1_l, tf_wave1_h], dim=1), rev=True), '(b t) c h w -> b t c h w', b=b)
 
         # reconstruction # 使用重建模块将特征图重建为去模糊后的图像
         out = rearrange(self.recons(rearrange(pro_feat, 'b t c h w -> b c t h w')), 'b c t h w -> b t c h w')
@@ -160,8 +164,9 @@ class manual_conv3d_propagation_backward(nn.Module):
         backward_list = []  # 初始化一个空列表，用于存储每个时间步的特征
         feat_prop = feature.new_zeros(b, c, h, w)   # 初始化一个特征图，用于存储传播的特征
         # propagation   # 从后向前遍历每个时间步，实现特征的反向传播
-        for i in range(t - 1, -1, -1):
-            x_feat = feature[:, i, :, :, :]     # 获取当前时间步的特征
+        '''这里的for循环是整个模块的核心部分，通过从后向前遍历每个时间步，实现特征的反向传播。feat_prop 是一个初始化为零的特征图，用于存储传播的特征。'''
+        for i in range(t - 1, -1, -1): # t-1到0，步长为-1
+            x_feat = feature[:, i, :, :, :]   # b 64 256 256  # 获取当前时间步的特征，feature[:, i, :, :, :]表示取feature的第i个时间步的特征
             # fusion propagation
             feat_fusion = torch.cat([x_feat, feat_prop], dim=1)       # b 128 256 256   # 将当前时间步的特征与传播的特征进行融合
             feat_fusion = self.lrelu(self.conv1(feat_fusion))   # b 128 256 256     # 通过卷积层和激活函数处理融合后的特征
@@ -214,7 +219,7 @@ class manual_conv3d_propagation_forward(nn.Module):
         self.conv1 = nn.Conv2d(num_feat * 2, num_feat * 2, 3, 1, 1, bias=True)
         self.conv2 = nn.Conv2d(num_feat, num_feat, 3, 1, 1, bias=True)
         self.conv3 = nn.Conv2d(num_feat, num_feat, 3, 1, 1, bias=True)
-        self.kernel_conv_pixel = IDynamicDWConv(num_feat, 3, 1, 4, 1)   # 动态像素卷积层，用于处理特征图
+        self.kernel_conv_pixel = IDynamicDWConv(num_feat, 3, 1, 4, 1)   # DTFF模块
         self.lrelu = nn.LeakyReLU(negative_slope=0.1, inplace=True) # 激活函数，使用LeakyReLU引入非线性
         self.resblock_bcakward2d = ResidualBlocks2D(num_feat, num_block)    # 2D残差块，用于提取特征
 
@@ -238,16 +243,17 @@ class manual_conv3d_propagation_forward(nn.Module):
             Tensor: 经过向前传播处理的特征图，形状为 (batch_size, time, channel, height, width)。
         """
         # predefine # 获取输入特征图的维度信息
-        b, t, c, h, w = feature.size()                          # b t 64 256 256
-        forward_list = []   # 初始化一个空列表，用于存储每个时间步的特征
+        b, t, c, h, w = feature.size()        # b t 64 256 256
+        forward_list = []   # 初始化一个空列表，用于存储每个时间步的特征。用法：forward_list.append(feat_prop)
         feat_prop = feature.new_zeros(b, c, h, w)   # 初始化一个特征图，用于存储传播的特征
+        '''通过从前向后遍历每个时间步，实现特征的正向传播。feat_prop是一个初始化为零的特征图，用于存储传播的特征。forward_list是一个空列表，用于存储所有时间步的特征。'''
         for i in range(0, t):   # 从前向后遍历每个时间步，实现特征的正向传播
             x_feat = feature[:, i, :, :, :] # 获取当前时间步的特征
             # fusion propagation
             feat_fusion = torch.cat([x_feat, feat_prop], dim=1)  # b 128 256 256    # 将当前时间步的特征与传播的特征进行融合
             feat_fusion = self.lrelu(self.conv1(feat_fusion))  # b 128 256 256  # 通过卷积层和激活函数处理融合后的特征
             feat_prop1, feat_prop2 = torch.split(feat_fusion, self.num_feat, dim=1) # 将融合后的特征分割成两部分，并分别通过卷积层和激活函数处理
-            feat_prop1 = feat_prop1 * torch.sigmoid(self.conv2(feat_prop1))
+            feat_prop1 = feat_prop1 * torch.sigmoid(self.conv2(feat_prop1)) # Element-wise Product
             feat_prop2 = feat_prop2 * torch.sigmoid(self.conv3(feat_prop2))
             feat_prop = feat_prop1 + feat_prop2 # 将处理后的特征进行融合，得到传播的特征
             # dynamic conv
